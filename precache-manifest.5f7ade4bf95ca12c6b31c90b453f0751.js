3161fbb8ed00592e5689449785f0dee78afbd1f9
techjini <techjini@its-MacBook-Pro.local>
2020-02-03 22:36:46 +0530

    Updates

---
+++ precache-manifest.5f7ade4bf95ca12c6b31c90b453f0751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff749aa3594ad8ef17c5d73a04cad25b",
+    "url": "/vipinpal/aboutme/index.html"
+  },
+  {
+    "revision": "139d33a0faad78bc798d",
+    "url": "/vipinpal/aboutme/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04b73cbeaa0783104fcc",
+    "url": "/vipinpal/aboutme/static/js/2.8648ef12.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/vipinpal/aboutme/static/js/2.8648ef12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139d33a0faad78bc798d",
+    "url": "/vipinpal/aboutme/static/js/main.5608891a.chunk.js"
+  },
+  {
+    "revision": "3119c6abec100b6e2f07",
+    "url": "/vipinpal/aboutme/static/js/runtime-main.040deaad.js"
+  }
+]);
